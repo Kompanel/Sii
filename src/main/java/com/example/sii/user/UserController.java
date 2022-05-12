f85f45a2b85b0e707d27f108559ffd8097565482
Kompanel <aloker1515@gmail.com>
2022-05-12 14:53:53 +0200

    Bug fix.
    method type in UserController

--- src/main/java/com/example/sii/user/UserController.java
+++ src/main/java/com/example/sii/user/UserController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class UserController {
 
   private final UserService userService;
 
-  @PostMapping("changeEmail")
+  @PutMapping("changeEmail")
   @Operation(summary = "change email")
   ResponseEntity<Object> changeEmail(@RequestBody UserChangingEmailDTO userChangingEmailDTO) {
     return userService.changeEmail(userChangingEmailDTO);
